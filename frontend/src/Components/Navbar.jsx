791153e476082625c771abc16dbc5768fecd8f4d
Ashutosh Dahal <137473283+EffectiveBear@users.noreply.github.com>
2025-05-21 20:55:50 +0545

    home-and-nav

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,16 +1,165 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import myImageIcon from "../assets/my-icon.jpg";
+import { Link } from "react-router-dom";
+import gsap from "gsap";
 
 const Navbar = () => {
-  return (
-    <nav className="">
-      {/* this will be the image that will travel */}
-      <div className="rounded-full border-1 border-black w-10 h-10 overflow-hidden ">
-        <img src={myImageIcon} alt="my image" className="min-w-full min-h-full hover:scale-105 transition ease-linear duration-300"/>
-      </div>
+  const navElementAnimate = useRef([]);
+  useEffect(() => {
+    gsap
+      .timeline()
+      .fromTo(
+        ".gsap-updown-custom-animate",
+        {
+          y: -10,
+          opacity: 0,
+        },
+        {
+          duration: 1,
+          y: -10,
+          opacity: 0,
+        }
+      )
+      .fromTo(
+        ".gsap-updown-custom-animate",
+        {
+          y: -10,
+          opacity: 0,
+        },
+        {
+          // duration: ,
+          y: 0,
+          opacity: 1,
+          stagger: 0.3,
+        }
+      );
 
-      <div>
-        <h3 className="">Ashu's World</h3>
+    gsap
+      .timeline()
+      .fromTo(
+        ".title-block-animation-gsap",
+        {
+          position: "absolute",
+          x: "50%",
+          y: "50%",
+          transform: "translate(-50%,-50%)",
+        },
+        {
+          position: "absolute",
+          x: "50%",
+          y: "50%",
+          transform: "translate(-50%,-50%)",
+          duration: 1.5,
+        }
+      )
+      .fromTo(
+        ".title-block-animation-gsap",
+        {
+          position: "absolute",
+          x: "50%",
+          y: "50%",
+          transform: "translate(-50%,-50%)",
+        },
+        {
+          position: "",
+          x: "",
+          y: "",
+          transform: "",
+        }
+      );
+    gsap
+      .timeline()
+      .fromTo(
+        ".title-each-letter-animation-gsap",
+        {
+          opacity: 0.6,
+          fontSize: "100px",
+          fontFamily: "League Spartan",
+          fontWeight: 200,
+          padding: "0 15px",
+          x: window.innerWidth / 4,
+          y: window.innerHeight / 4,
+        },
+        {
+          x: window.innerWidth / 4,
+          y: window.innerHeight / 4,
+          opacity: 0,
+          duration: 1.5,
+        }
+      )
+      .set(".title-each-letter-animation-gsap", {
+        fontSize: "",
+        fontFamily: "",
+        fontWeight: "",
+        padding: "",
+      })
+      .fromTo(
+        ".title-each-letter-animation-gsap",
+        {
+          opacity: 1,
+          color: "#ffffff",
+          x: window.innerWidth / 4,
+          y: window.innerHeight / 4,
+        },
+        {
+          duration: 1,
+          opacity: 1,
+          x: "",
+          y: "",
+          color: "",
+          stagger: 0.1,
+          fontSize: "",
+        }
+      );
+  }, []);
+
+  return (
+    <nav
+      className="flex justify-between items-center shadow-sm bg-custom-color"
+      style={{ backgroundColor: "#f3eeee9c" }}
+    >
+      {/* this will be the image that will travel */}
+      <div className="flex justify-start items-center mx-4 h-[55px]">
+        <div className="rounded-full border-2 border-black w-8 h-8 overflow-hidden mx-2">
+          <img
+            src={myImageIcon}
+            alt="my image"
+            className="min-w-[90%] min-h-[90%] hover:scale-105 transition ease-linear duration-300 "
+          />
+        </div>
+        <Link to="/">
+          {" "}
+          <div className="flex title-block-animation-gsap z-10 mt-1">
+            {"Ashu's World".split("").map((letter) => {
+              return (
+                <p className="bad-script-custom-font text-gray-700 font-bold text-2xl px-1 cursor-pointer whitespace-pre title-each-letter-animation-gsap opacity-0">
+                  {letter}
+                </p>
+              );
+            })}
+          </div>
+        </Link>
+      </div>
+      <div className="mr-20">
+        <ul className="flex justify-center items-center gap-10">
+          {/*this is to compensate for the stagger effect..it staggger onnly for other not for this*/}
+          <li className="hidden gsap-updown-custom-animate"></li>{" "}
+          <Link to="/">
+            <li className="gsap-updown-custom-animate -translate-y-4 opacity-0 cursor-pointer font-semibold text-gray-600 hover:text-black text-lg transition-all hover:scale-105  duration-300">
+              Home
+            </li>
+          </Link>
+          <Link to="/vault">
+            <li className="gsap-updown-custom-animate -translate-y-4 opacity-0 cursor-pointer font-semibold text-gray-600 hover:text-black text-lg transition-all hover:scale-105 duration-300">
+              Vault
+            </li>
+          </Link>
+          <Link to="/contact">
+            <li className="gsap-updown-custom-animate -translate-y-4 opacity-0 cursor-pointer font-semibold text-gray-600 hover:text-black text-lg transition-all hover:scale-105 duration-300">
+              Contact
+            </li>
+          </Link>
+        </ul>
       </div>
     </nav>
   );
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
-
+import React, { useEffect, useRef } from "react";
+import backgroundImageInHome from "../assets/home-bg-1.jpg";
+import gsap from "gsap";
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const bgImageRef = useRef(null);
+  const bgBigTextRef = useRef(null);
+  const bgSmallTextRef = useRef(null);
 
-export default Home
+  useEffect(() => {
+    gsap
+      .timeline()
+      .fromTo(
+        bgImageRef.current,
+        { scale: 0.85, opacity: 0, filter: "blur(10px)" },
+        { scale: 1, opacity: 1, duration: 1 }
+      )
+      .to(bgImageRef.current, { duration: 1, scale: 1, filter: "blur(2px)" });
+
+    gsap
+      .timeline()
+      .from(bgBigTextRef, {
+        x: 0,
+        opacity: 1,
+        duration: 1,
+      })
+      .fromTo(
+        bgBigTextRef.current,
+        { x: -20, opacity: 0 },
+        { x: 0, opacity: 1, duration: 0.3 }
+      )
+      .fromTo(
+        bgSmallTextRef.current,
+        { x: 20, opacity: 0 },
+        { x: 0, opacity: 1, duration: 0.3 }
+      );
+  }, []);
+
+  return (
+    <>
+      <div className="flex justify-center items-center">
+        <div className="w-[94vw] h-[94vh] flex flex-col justify-center items-center overflow-hidden relative ">
+          <div
+            className="w-full h-full overflow-hidden shadow-2xl "
+            ref={bgImageRef}
+          >
+            <img
+              src={backgroundImageInHome}
+              alt=""
+              className="w-full h-full object-cover  transition-all duration-2000 hover:scale-110 ease-linear "
+            />
+          </div>
+          <div className="absolute top-1/4 flex flex-col items-center">
+            <h3
+              ref={bgBigTextRef}
+              className="text-2xl sm:text-6xl league-spartan font-extralight whitespace-pre text-custom-color "
+            >
+              {"  "}A S H U T O S H{"    "} D A H A L
+            </h3>
+            <p
+              ref={bgSmallTextRef}
+              className="text-sm sm:text-2xl  my-2 sm:my-6 league-spartan font-extralight whitespace-pre text-custom-color mx-auto"
+            >
+              "{"  "}E X C I T E D{"      "}T O{"      "}L E A R N ,{"      "}E
+              N T H U S I A S T I C{"      "}T O{"      "}B U I L D "
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
